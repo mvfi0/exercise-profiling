899da72c2200820e4283b35fed0fa680f20d8235
mvfi0 <muhammad.vegard@ui.ac.id>
2026-04-28 21:05:33 +0700

    [Refactoring] - Optimizing method joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -2,7 +2,10 @@ package com.advpro.profiling.tutorial.repository;
 
 import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -10,5 +13,7 @@ import java.util.Optional;
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s.name FROM Student s")
+    List<String> findAllNames();
     Optional<Student> findTopByOrderByGpaDesc();
 }
